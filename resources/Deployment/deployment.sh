401a6360358802579327ddff48e195b930ab58b3
Abhishek Singh <abhishek11801529@gmail.com>
2024-02-24 19:43:22 +0530

    gcloud update

--- resources/Deployment/deployment.sh
+++ resources/Deployment/deployment.sh
@@ -5,15 +5,11 @@ $FOLDER_PATH = 'resources/Storage/apcha.csv'
 # Set project ID and region from Project Secrets
 read -r PROJECT_NAME REGION <<< $(echo "$1" | tr ',' ' ')
 
-# Set default project and region
-gcloud config set project $PROJECT_NAME
-gcloud config set compute/region $REGION
-
 # Set GCS Variables from GCS Secrets
 read -r BUCKET_NAME GCS_FOLDER <<< $(echo "$2" | tr ',' ' ')
 
 # Create GCS bucket
-gsutil mb gs://$BUCKET_NAME
+gcloud storage buckets create gs://$BUCKET_NAME --project=$PROJECT_NAME --location=$REGION --no-public-access-prevention --no-uniform-bucket-level-access
 
 # cp local folder to GCS bucket
-gsutil -m cp -r $FOLDER_PATH gs://$BUCKET_NAME/$GCS_FOLDER
+gsutil -m cp $FOLDER_PATH gs://$BUCKET_NAME/$GCS_FOLDER
