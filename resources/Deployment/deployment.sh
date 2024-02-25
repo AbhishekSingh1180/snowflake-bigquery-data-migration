f9020e5ebf9ad17f9e8fb929568ae8263cd572e1
Abhishek Singh <abhishek11801529@gmail.com>
2024-02-25 18:57:22 +0530

    sh fix 1

--- resources/Deployment/deployment.sh
+++ resources/Deployment/deployment.sh
@@ -17,11 +17,7 @@ read -r DATASET_NAME TABLE_NAME <<< $(echo "$4" | tr ',' ' ')
 # # STEP 1 : Setup GCS bucket
 
 # Create GCS bucket
-gcloud storage buckets create gs://$BUCKET_NAME / 
---project=$PROJECT_NAME / 
---location=$REGION / 
---public-access-prevention / 
---uniform-bucket-level-access
+gcloud storage buckets create gs://$BUCKET_NAME --project=$PROJECT_NAME  --location=$REGION --public-access-prevention --uniform-bucket-level-access
 
 # CP setup files to GCS bucket
 gcloud storage cp -r $FOLDER_PATH gs://$BUCKET_NAME/
@@ -34,16 +30,16 @@ echo "$(date) - ARCHIVE" | gcloud storage cp - gs://$BUCKET_NAME/$GCS_ARCHIVE_FO
 
 EXECUTE_SQL_SCRIPT="execute.sql" 
 
-echo "CREATE STORAGE INTEGRATION GCS_STORAGE_INT /
-  TYPE = EXTERNAL_STAGE /
-  STORAGE_PROVIDER = 'GCS' /
-  ENABLED = TRUE /
+echo "CREATE STORAGE INTEGRATION GCS_STORAGE_INT \
+  TYPE = EXTERNAL_STAGE \
+  STORAGE_PROVIDER = 'GCS' \
+  ENABLED = TRUE \
   STORAGE_ALLOWED_LOCATIONS = ('gcs://$BUCKET_NAME/$GCS_SINK_FOLDER/');" 
 >> $EXECUTE_SQL_SCRIPT
 
-echo "CREATE STAGE FINANCE_DB.DW_APPL.SENSEX_DATA_STAGE_OUT /
-  URL = 'gcs://$BUCKET_NAME/$GCS_SINK_FOLDER/' /
-  STORAGE_INTEGRATION = GCS_STORAGE_INT /
+echo "CREATE STAGE FINANCE_DB.DW_APPL.SENSEX_DATA_STAGE_OUT \
+  URL = 'gcs://$BUCKET_NAME/$GCS_SINK_FOLDER/' \
+  STORAGE_INTEGRATION = GCS_STORAGE_INT \
   FILE_FORMAT = (TYPE = CSV SKIP_HEADER = 1);"
 >> $EXECUTE_SQL_SCRIPT
 
@@ -51,7 +47,7 @@ echo "CREATE STAGE FINANCE_DB.DW_APPL.SENSEX_DATA_STAGE_OUT /
 rm -rf $EXECUTE_SQL_SCRIPT
 
 # Retrieve service account
-SNF_SERVICE_ACCOUNT=$(~/bin/snowsql --config ~/.snowsql/config --connection awesome -w "COMPUTE_WH" -q "DESC STORAGE INTEGRATION GCS_STORAGE_INT" /
+SNF_SERVICE_ACCOUNT=$(~/bin/snowsql --config ~/.snowsql/config --connection awesome -w "COMPUTE_WH" -q "DESC STORAGE INTEGRATION GCS_STORAGE_INT" \
 -o output_format=csv -o header=false | awk 'NR==7' | cut -d',' -f3 | tr -d '"' )
 
 gcloud iam roles create $CUSTOM_ROLE /
@@ -61,8 +57,7 @@ gcloud iam roles create $CUSTOM_ROLE /
     --permissions=$PERMISION_1,$PERMISION_2,$PERMISION_3
 
 #
-gcloud storage buckets add-iam-policy-binding gs://$BUCKET_NAME --member=serviceAccount:$SNF_SERVICE_ACCOUNT /
- --role=projects/$PROJECT_NAME/roles/$CUSTOM_ROLE --project=$PROJECT_NAME 
+gcloud storage buckets add-iam-policy-binding gs://$BUCKET_NAME --member=serviceAccount:$SNF_SERVICE_ACCOUNT --role=projects/$PROJECT_NAME/roles/$CUSTOM_ROLE --project=$PROJECT_NAME 
 
 #----------------------------------------------------------------------------------------------------------
 
