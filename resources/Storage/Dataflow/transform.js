8a73c6876f0c699a74e68fa103179a306961941b
Abhishek Singh <abhishek11801529@gmail.com>
2024-02-26 06:37:54 +0530

    commit dataflow changes

--- resources/Storage/Dataflow/transform.js
+++ resources/Storage/Dataflow/transform.js
@@ -1,17 +1,17 @@
 function transform(line) {
 
-    if (line.trim().toLowerCase().startsWith('index')) {
+    if (line.trim().toLowerCase().startsWith('CLOSE')) {
         return;
     }
     var values = line.split(',');
     var obj = {};
-    obj.INDEX = values[0];
-    obj.SYMBOL = values[1];
-    obj.TIMESTAMP = values[2];
-    obj.OPEN = parseFloat(values[3]);
-    obj.CLOSE = parseFloat(values[4]);
-    obj.HIGH = parseFloat(values[5]);
-    obj.LOW = parseFloat(values[6]);
+    obj.CLOSE = parseFloat(values[0]);
+    obj.HIGH = parseFloat(values[1]);
+    obj.INDEX = values[2];
+    obj.LOW = parseFloat(values[3]);
+    obj.OPEN = parseFloat(values[4]);
+    obj.SYMBOL = values[5];
+    obj.TIMESTAMP = values[6];
     obj.VOLUME = parseInt(values[7]);
 
     var jsonString = JSON.stringify(obj);
